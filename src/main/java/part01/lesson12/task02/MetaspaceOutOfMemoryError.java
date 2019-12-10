e2ef373122f5b1f90dfaef6ba95e91a85bf70a9b
Oleg Chapurin <oleg_chapurin@mail.ru>
2019-12-11 01:55:32 +0400

    part01 lesson12 task02 variant with loader

--- src/main/java/part01/lesson12/task02/MetaspaceOutOfMemoryError.java
+++ src/main/java/part01/lesson12/task02/MetaspaceOutOfMemoryError.java
@@ -1,7 +1,7 @@
 package part01.lesson12.task02;
 
-import javassist.CannotCompileException;
-import javassist.ClassPool;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * -XX:MaxMetaspaceSize=512m
@@ -12,16 +12,29 @@ import javassist.ClassPool;
 public class MetaspaceOutOfMemoryError {
 
     /**
-     * -XX:MaxMetaspaceSize=512m
+     * -XX:MaxMetaspaceSize=128m
      * return OutOfMemoryError Metaspace
      */
 
     public static void main(String[] args) {
-        ClassPool cp = ClassPool.getDefault();
+        List<Class> list = new ArrayList<>();
+        /** Compiler */
+        MyCompiler myCompiler = new MyCompiler();
+        myCompiler.setListFiles("src\\main\\java\\part01\\lesson12\\task02\\ReadWriteConsole.java");
+        myCompiler.compiler();
+        String fileSource = "src\\main\\java\\part01\\lesson12\\task02\\ReadWriteConsole.class";
         for (int i = 0; i < 100000000; i++){
+            ClassLoader l = new MyClassLoader("part01.lesson12.task02.ReadWriteConsole",fileSource);
             try {
-                Class c = cp.makeClass("part01.lesson12.task02.ReadWriteConsole" + i).toClass();
-            } catch (CannotCompileException e) {
+                Class<?> someClass = l.loadClass("part01.lesson12.task02.ReadWriteConsole");
+                list.add(someClass);
+                if ((i % 5) == 0) {
+                    if (list.size() > 2) {
+                        list.remove(1);
+                    }
+                }
+                    Thread.sleep(1);
+            } catch (ClassNotFoundException | InterruptedException e) {
                 e.printStackTrace();
             }
         }
